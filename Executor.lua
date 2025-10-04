2ef8c85ae1a3b86d4856d56f730446e9f67ca115
TegarPX <tegarputracloud@gmail.com>
2025-10-04 14:07:24 +0700

    Create Executor.lua

---
+++ Executor.lua
@@ -0,0 +1,16 @@
+
+
+
+
+AddHook('OnSendPacket', 0101, function(type, str)
+    if str:find("/epep") then
+      return true
+    end
+    if str:find("/proxy") then
+      LogToConsole(server_ver)
+      LogToConsole(server_name)
+      LogToConsole(server_teks)
+      return true
+    end
+    return false
+  end)
